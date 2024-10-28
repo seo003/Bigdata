f26908de2f2314b7fad10115c1c03606e9bf58eb
seo003 <sy030212@naver.com>
2024-10-28 12:05:39 +0900

    241028

--- news_crawling.py
+++ news_crawling.py
@@ -6,88 +6,87 @@ import time
 import json
 
 
-# 클라이언트 ID와 비밀번호를 JSON 파일에서 읽어오기
-def load_client_credentials(filename='/content/drive/MyDrive/Colab Notebooks/project/bigdata/naver_client.json'):
+# client id, pw를 JSON 파일에서 읽어오기
+def load_client_info(filename='naver_client.json'):
     with open(filename, 'r') as f:
         credentials = json.load(f)
     return credentials['client_id'], credentials['client_secret']
 
-# 클라이언트 ID와 비밀번호 로드
-client_id, client_secret = load_client_credentials()
+
+# client 정보 로드
+client_id, client_pw = load_client_info()
 
 
-#[CODE 1]
 def getRequestUrl(url):
     req = urllib.request.Request(url)
     req.add_header("X-Naver-Client-Id", client_id)
-    req.add_header("X-Naver-Client-Secret", client_secret)
+    req.add_header("X-Naver-Client-Secret", client_pw)
 
     try:
         response = urllib.request.urlopen(req)
         if response.getcode() == 200:
-            print ("[%s] Url Request Success" % datetime.datetime.now())
+            print("[%s] Url Request Success" % datetime.datetime.now())
             return response.read().decode('utf-8')
     except Exception as e:
         print(e)
         print("[%s] Error for URL : %s" % (datetime.datetime.now(), url))
         return None
 
-#[CODE 2]
+
 def getNaverSearch(node, srcText, start, display):
     base = "https://openapi.naver.com/v1/search"
     node = "/%s.json" % node
     parameters = "?query=%s&start=%s&display=%s" % (urllib.parse.quote(srcText), start, display)
 
     url = base + node + parameters
-    responseDecode = getRequestUrl(url)   #[CODE 1]
+    responseDecode = getRequestUrl(url)
 
     if (responseDecode == None):
         return None
     else:
         return json.loads(responseDecode)
 
-#[CODE 3]
+
 def getPostData(post, jsonResult, cnt):
     title = post['title']
     description = post['description']
-    org_link = post['originallink']
     link = post['link']
 
-    pDate = datetime.datetime.strptime(post['pubDate'],  '%a, %d %b %Y %H:%M:%S +0900')
-    pDate = pDate.strftime('%Y-%m-%d %H:%M:%S')
+    pDate = datetime.datetime.strptime(post['pubDate'], '%a, %d %b %Y %H:%M:%S +0900')
+    pDate = pDate.strftime('%Y-%m-%d')
 
-    jsonResult.append({'cnt':cnt, 'title':title, 'description': description, 'org_link':org_link,   'link': org_link,   'pDate':pDate})
+    jsonResult.append({'cnt': cnt, 'title': title, 'description': description, 'link': link, 'pDate': pDate})
     return
 
-#[CODE 0]
+
 def main():
-    node = 'news'   # 크롤링 할 대상
-    srcText = "축제"
+    node = 'news'
+    topic = "축제"
+
     cnt = 0
     jsonResult = []
 
-    jsonResponse = getNaverSearch(node, srcText, 1, 100)  #[CODE 2]
+    jsonResponse = getNaverSearch(node, topic, 1, 100)
     total = jsonResponse['total']
 
     while ((jsonResponse != None) and (jsonResponse['display'] != 0)):
         for post in jsonResponse['items']:
             cnt += 1
-            getPostData(post, jsonResult, cnt)  #[CODE 3]
+            getPostData(post, jsonResult, cnt)
 
         start = jsonResponse['start'] + jsonResponse['display']
-        jsonResponse = getNaverSearch(node, srcText, start, 100)  #[CODE 2]
+        jsonResponse = getNaverSearch(node, topic, start, 100)
 
-    print('전체 검색 : %d 건' %total)
+    print('전체 검색 : %d 건' % total)
 
-    output_file = '/content/drive/MyDrive/Colab Notebooks/project/bigdata/%s_naver_%s.json' % (srcText, node)
+    output_file = '%s_naver_%s.json' % (topic, node)
 
     with open(output_file, 'w', encoding='utf8') as outfile:
-        jsonFile = json.dumps(jsonResult,  indent=4, sort_keys=True,  ensure_ascii=False)
+        jsonFile = json.dumps(jsonResult, indent=4, sort_keys=True, ensure_ascii=False)
         outfile.write(jsonFile)
 
+    print('데이터 크롤링 완료' % (topic, node))
 
-    print("가져온 데이터 : %d 건" %(cnt))
-    print ('%s_naver_%s.json SAVED' % (srcText, node))
 
 if __name__ == '__main__':
     main()
